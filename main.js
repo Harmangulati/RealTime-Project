1d88e3db292bd37358c0cf72e90445ebafd74052
Harmangulati <83772120+Harmangulati@users.noreply.github.com>
2022-02-02 16:47:09 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+
+difference=0;
+leftWristX=0;
+rightWristX=0;
 function setup(){
   canvas=createCanvas(550,550);
   canvas.position(560,150);
@@ -9,11 +13,20 @@ poseNet.on('pose',gotPoses);
 function modelLoaded(){
   console.log("PoseNet is Initialized");  
 }
-function gotPoses(){
+function gotPoses(results){
     if(results.length>0){
-     console.log(results);   
+     console.log(results); 
+  leftWristX=results[0].pose.leftWrist.x; 
+  rightWristX=results[0].pose.rightWrist.x; 
+  difference= floor(leftWristX-rightWristX);
+  console.log("leftWristX= "+leftWristX+" rightWristX= "+rightWristX+" difference= "+difference);  
     }
 }
 function draw(){
     background('#656e67')
+      document.getElementById("text").innerHTML="Width and Height of text will be= "+difference+"px";
+      textSize(difference);
+      fill("#21dbd2");
+      stroke("#21dbd2");
+    text('Harman',50,400);
 }
